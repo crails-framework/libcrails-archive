212d905319689b7e2df287a71856d8d8ef81bc74
plaristote <michael@unetresgrossebite.com>
2022-12-03 18:36:25 +0100

    API update on libcrails-renderer

--- libcrails-archive/crails/renderers/archive_renderer.cpp
+++ libcrails-archive/crails/renderers/archive_renderer.cpp
@@ -13,8 +13,7 @@ bool ArchiveRenderer::can_render(const std::string& accept_header, const std::st
 void ArchiveRenderer::render_template(const std::string& view, RenderTarget& response, SharedVars& vars) const
 {
   auto tpl = templates.find(view);
-  std::string body = (*tpl).second(this, vars);
-
+  
   response.set_header("Content-Type", Archive::mimetype);
-  response.set_body(body.c_str(), body.length());
+  (*tpl).second(*this, response, vars);
 }
